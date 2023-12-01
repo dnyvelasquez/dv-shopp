1dbdc7df30fbe0c91a13ba6e21333ae3c1aed2b5
Daniel <dny.velasquez@gmail.com>
2023-11-30 22:07:34 -0500

    Log in

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,136 +1,203 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { ShoppingCartContext } from "../../Context";
-import { ShoppingBagIcon } from '@heroicons/react/24/solid';
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
 import logo from "../../assets/DVShark.svg";
 
 const NavBar = () => {
-    const context = useContext(ShoppingCartContext);
-    const activeStyle = 'underline underline-offset-4'
+  const context = useContext(ShoppingCartContext);
+  const activeStyle = "underline underline-offset-4";
+  
+  const signOut = localStorage.getItem("sign-out");
+  const parsedSignOut = JSON.parse(signOut);
+  const isUserSignOut = context.signOut;
 
-    return (
-        <nav className="flex items-center fixed z-10 top-0 w-full h-16 text-sm font-light border-b-2 nav-bar px-4 justify-between">
-            <div className="flex">
-                <img 
-                    src={logo} 
-                    alt="logo" 
-                    className="w-16 h-16 object-containt" 
-                />
-                <h1 className="font-semibold text-lg flex items-center">
-                    <NavLink 
-                        to='/dv-shopp/'
-                        onClick={() => context.setSearchByCategory()}
-                        >
-                        DV-Shopp
-                    </NavLink>
-                </h1>
-                <p 
-                    className="cursor-pointer md:hidden place-self-center ml-4"
-                    onClick={() => {context.isCategoriesMenuOpen 
-                        ? context.closeCategoriesMenu() 
-                        : context.openCategoriesMenu()
-                }}
-                >
-                    Categories
-                </p>
-                <div className={`${context.isCategoriesMenuOpen ? 'relative' : 'flex'}`}>
-                    <ul className={`
-                        ${context.isCategoriesMenuOpen 
-                            ? 'absolute -left-20 top-12 bg-white p-2 rounded-lg' 
-                            : 'hidden'
-                        } md:flex items-center gap-3 ml-4`}>
-                        <li>
-                            <NavLink 
-                                to='/dv-shopp/' 
-                                className={({ isActive }) => isActive ? activeStyle : undefined}
-                                onClick={() => context.setSearchByCategory()}
-                            >
-                                All
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to='/dv-shopp/clothes' 
-                                onClick={() => context.setSearchByCategory('clothes')}
-                                className={({ isActive }) => isActive ? activeStyle : undefined}                        
-                            >
-                                Clothes
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to='/dv-shopp/electronics' 
-                                onClick={() => context.setSearchByCategory('electronics')}
-                                className={({ isActive }) => isActive ? activeStyle : undefined}
-                            >
-                                Electronic
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to='/dv-shopp/furnitures' 
-                                onClick={() => context.setSearchByCategory('furnitures')}
-                                className={({ isActive }) => isActive ? activeStyle : undefined}
-                            >
-                                Furnitures
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to='/dv-shopp/toys' 
-                                onClick={() => context.setSearchByCategory('toys')}
-                                className={({ isActive }) => isActive ? activeStyle : undefined}
-                            >
-                                Toys
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to='/dv-shopp/others' 
-                                onClick={() => context.setSearchByCategory('others')}
-                                className={({ isActive }) => isActive ? activeStyle : undefined}
-                            >
-                                Others
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <ul className='flex gap-2'>
-                <li>
-                    <NavLink to='mailto:dny.velasquez@gmail.com' className={({ isActive }) => isActive ? activeStyle : undefined}>
-                        <span className="hidden sm:flex text-sm">dny.velasquez@gmail.com</span> 
-                        <span className="flex sm:hidden text-lg font-bold">@</span> 
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/dv-shopp/my-orders' className={({ isActive }) => isActive ? activeStyle : undefined}>
-                        My orders
-                    </NavLink>
-                </li>
-                {/* <li>
-                    <NavLink to='/my-account' className={({ isActive }) => isActive ? activeStyle : undefined}>
-                        My account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/sign-in' className={({ isActive }) => isActive ? activeStyle : undefined}>
-                        Sign in
-                    </NavLink>
-                </li> */}
-                <li 
-                    className="flex items-center cursor-pointer"
-                    onClick={() => {
-                        context.openCheckoutSideMenu();
-                        context.closeProductDetail();                    
-                    }}
-                >
-                    <ShoppingBagIcon className="h-6 w-6 text-black"/>
-                    <div>{context.cartProducts.length}</div>                    
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  const account = localStorage.getItem("account");
+  const parsedAccount = JSON.parse(account);
+
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+
+  const handleSignOut = () => {
+    const stringfiedSignOut = JSON.stringify(true);
+    localStorage.setItem("sign-out", stringfiedSignOut);
+    context.setSignOut(true);
+  };
+
+  const renderView = () => {
+    if (isUserSignOut) {
+      return (
+        <li>
+          <NavLink
+            to="/dv-shopp/sign-in"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Sign in
+          </NavLink>
+        </li>
+      );
+    } else {
+      return (
+        <>
+          <li>
+            <NavLink
+              to="mailto:dny.velasquez@gmail.com"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              <span className="hidden sm:flex text-sm">
+                {parsedAccount?.email}
+              </span>
+              <span className="flex sm:hidden text-lg font-bold">@</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dv-shopp//my-account"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              My account
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dv-shopp/my-orders"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              My orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dv-shopp/"
+              onClick={() => handleSignOut()}
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Sign out
+            </NavLink>
+          </li>
+        </>
+      );
+    }
+  };
+
+  return (
+    <nav className="flex items-center fixed z-10 top-0 w-full h-16 text-sm font-light border-b-2 nav-bar px-4 justify-between">
+      <div className="flex">
+        <img src={logo} alt="logo" className="w-16 h-16 object-containt" />
+        <h1 className="font-semibold text-lg flex items-center">
+          <NavLink
+            to={`${isUserSignOut ? '/dv-shopp/signIn' : ' /dv-shopp/'}`}
+            onClick={() => context.setSearchByCategory()}
+          >
+            DV-Shopp
+          </NavLink>
+        </h1>
+        <p
+          className="cursor-pointer md:hidden place-self-center ml-4"
+          onClick={() => {
+            context.isCategoriesMenuOpen
+              ? context.closeCategoriesMenu()
+              : context.openCategoriesMenu();
+          }}
+        >
+          Categories
+        </p>
+        <div
+          className={`${context.isCategoriesMenuOpen ? "relative" : "flex"}`}
+        >
+          <ul
+            className={`
+                        ${
+                          context.isCategoriesMenuOpen
+                            ? "absolute -left-20 top-12 bg-white p-2 rounded-lg"
+                            : "hidden"
+                        } md:flex items-center gap-3 ml-4`}
+          >
+            <li>
+              <NavLink
+                to="/dv-shopp/"
+                className={({ isActive }) =>
+                  isActive ? activeStyle : undefined
+                }
+                onClick={() => context.setSearchByCategory()}
+              >
+                All
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dv-shopp/clothes"
+                onClick={() => context.setSearchByCategory("clothes")}
+                className={({ isActive }) =>
+                  isActive ? activeStyle : undefined
+                }
+              >
+                Clothes
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dv-shopp/electronics"
+                onClick={() => context.setSearchByCategory("electronics")}
+                className={({ isActive }) =>
+                  isActive ? activeStyle : undefined
+                }
+              >
+                Electronic
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dv-shopp/furnitures"
+                onClick={() => context.setSearchByCategory("furnitures")}
+                className={({ isActive }) =>
+                  isActive ? activeStyle : undefined
+                }
+              >
+                Furnitures
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dv-shopp/toys"
+                onClick={() => context.setSearchByCategory("toys")}
+                className={({ isActive }) =>
+                  isActive ? activeStyle : undefined
+                }
+              >
+                Toys
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dv-shopp/others"
+                onClick={() => context.setSearchByCategory("others")}
+                className={({ isActive }) =>
+                  isActive ? activeStyle : undefined
+                }
+              >
+                Others
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <ul className="flex gap-2">
+        {renderView()}
+        <li
+          className="flex items-center cursor-pointer"
+          onClick={() => {
+            context.openCheckoutSideMenu();
+            context.closeProductDetail();
+          }}
+        >
+          <ShoppingBagIcon className="h-6 w-6 text-black" />
+          <div>{context.cartProducts.length}</div>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,105 +4,113 @@ export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({children}) => {
 
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    const [isProductDetailOpen, setProductDetailOpen] = useState(false);
-    const openProductDetail = () => {setProductDetailOpen(true)}
-    const closeProductDetail = () => {setProductDetailOpen(false)}    
+  const [isProductDetailOpen, setProductDetailOpen] = useState(false);
+  const openProductDetail = () => {setProductDetailOpen(true)}
+  const closeProductDetail = () => {setProductDetailOpen(false)}    
 
-    const [isCheckoutSideMenuOpen, setCheckoutSideMenuOpen] = useState(false);
-    const openCheckoutSideMenu = () => {setCheckoutSideMenuOpen(true)}
-    const closeCheckoutSideMenu = () => {setCheckoutSideMenuOpen(false)}    
+  const [isCheckoutSideMenuOpen, setCheckoutSideMenuOpen] = useState(false);
+  const openCheckoutSideMenu = () => {setCheckoutSideMenuOpen(true)}
+  const closeCheckoutSideMenu = () => {setCheckoutSideMenuOpen(false)}    
 
-    const [isCategoriesMenuOpen, setCategoriesMenuOpen] = useState(false);
-    const openCategoriesMenu = () => {setCategoriesMenuOpen(true)}
-    const closeCategoriesMenu = () => {setCategoriesMenuOpen(false)}    
+  const [isCategoriesMenuOpen, setCategoriesMenuOpen] = useState(false);
+  const openCategoriesMenu = () => {setCategoriesMenuOpen(true)}
+  const closeCategoriesMenu = () => {setCategoriesMenuOpen(false)}    
 
-    const [productToShow, setProductToShow] = useState({});
-    const [cartProducts, setCartProducts] = useState([]);
-    const [order, setOrder] = useState([]);
+  const [productToShow, setProductToShow] = useState({});
+  const [cartProducts, setCartProducts] = useState([]);
+  const [order, setOrder] = useState([]);
 
-    const [items, setItems] = useState(null);
-    const [filteredItems, setFilteredItems] = useState(null);
-    
-    const [searchByTitle, setSearchByTitle] = useState(null);
-    const [searchByCategory, setSearchByCategory] = useState(null);
+  const [items, setItems] = useState(null);
+  const [filteredItems, setFilteredItems] = useState(null);
+  
+  const [searchByTitle, setSearchByTitle] = useState(null);
+  const [searchByCategory, setSearchByCategory] = useState(null);
 
-    useEffect(() => {
-        fetch('https://api.escuelajs.co/api/v1/products')
-        .then(response => response.json())
-        .then(data => setItems(data))
-    }, [])
-    
-    const filteredItemsByTitle = (items, searchByTitle) => {
-        return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  useEffect(() => {
+      fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+  
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
+
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }    
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }    
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }    
+    if (!searchType) {
+      return items
+    }
+  }
+
+   const [account, setAccount]  = useState({});
+
+   const [signOut, setSignOut] = useState(true);
+
+  useEffect(() => {
+      if (searchByTitle && searchByCategory) {
+          setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
       }
-    
-      const filteredItemsByCategory = (items, searchByCategory) => {
-        return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+      if (searchByTitle && !searchByCategory) {
+          setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
       }
-    
-      const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
-        if (searchType === 'BY_TITLE') {
-          return filteredItemsByTitle(items, searchByTitle)
-        }    
-        if (searchType === 'BY_CATEGORY') {
-          return filteredItemsByCategory(items, searchByCategory)
-        }    
-        if (searchType === 'BY_TITLE_AND_CATEGORY') {
-          return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
-        }    
-        if (!searchType) {
-          return items
-        }
+      if (!searchByTitle && searchByCategory) {
+          setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
       }
-    
-    useEffect(() => {
-        if (searchByTitle && searchByCategory) {
-            setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
-        }
-        if (searchByTitle && !searchByCategory) {
-            setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
-        }
-        if (!searchByTitle && searchByCategory) {
-            setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
-        }
-        if (!searchByTitle && !searchByCategory) {
-            setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
-        }
-    }, [items, searchByTitle, searchByCategory])
-    
-    return (
-        <ShoppingCartContext.Provider value={{
-            count, 
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            productToShow,
-            setProductToShow,
-            cartProducts, 
-            setCartProducts,
-            isCheckoutSideMenuOpen, 
-            setCheckoutSideMenuOpen,
-            openCheckoutSideMenu,
-            closeCheckoutSideMenu,
-            order,
-            setOrder,
-            items, 
-            setItems,
-            searchByTitle, 
-            setSearchByTitle,
-            filteredItems, 
-            setFilteredItems,
-            searchByCategory, 
-            setSearchByCategory,
-            isCategoriesMenuOpen, 
-            setCategoriesMenuOpen,
-            openCategoriesMenu,
-            closeCategoriesMenu,
-        }}>
-            {children};
-        </ShoppingCartContext.Provider>    
+      if (!searchByTitle && !searchByCategory) {
+          setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+      }
+  }, [items, searchByTitle, searchByCategory])
+  
+  return (
+      <ShoppingCartContext.Provider value={{
+          count, 
+          setCount,
+          openProductDetail,
+          closeProductDetail,
+          isProductDetailOpen,
+          productToShow,
+          setProductToShow,
+          cartProducts, 
+          setCartProducts,
+          isCheckoutSideMenuOpen, 
+          setCheckoutSideMenuOpen,
+          openCheckoutSideMenu,
+          closeCheckoutSideMenu,
+          order,
+          setOrder,
+          items, 
+          setItems,
+          searchByTitle, 
+          setSearchByTitle,
+          filteredItems, 
+          setFilteredItems,
+          searchByCategory, 
+          setSearchByCategory,
+          isCategoriesMenuOpen, 
+          setCategoriesMenuOpen,
+          openCategoriesMenu,
+          closeCategoriesMenu,
+          account, 
+          setAccount,
+          signOut,
+          setSignOut,
+      }}>
+          {children};
+      </ShoppingCartContext.Provider>    
     );
 };
